badf6b04a4bb0e6280dc4af08e5132f55f171bd5
vaja <74463035+vaja2000@users.noreply.github.com>
2023-05-22 01:42:54 +0400

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseCollection: {
+    service: "services"
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyDoNVA9c9zFL0XCMYLlvPn_0ZE3C7Quec0",
+    authDomain: "cyber-space-d25c6.firebaseapp.com",
+    databaseURL: "https://cyber-space-d25c6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cyber-space-d25c6",
+    storageBucket: "cyber-space-d25c6.appspot.com",
+    messagingSenderId: "248755452199",
+    appId: "1:248755452199:web:eb3c03418d926ac0281102"
+  }
 };
